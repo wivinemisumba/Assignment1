ea29e99b56b81def2cb96c0739675ea2415beb68
Wivinem <ilmisva@gmail.com>
2020-10-04 21:08:35 -0500

    initial commit

---
+++ VotingBox.java
@@ -0,0 +1,71 @@
+
+
+import stanford.karel.*;
+
+public class VotingBox extends SuperKarel {
+
+	public void run() {
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		
+		
+		
+				
+	}
+	
+		
+}
